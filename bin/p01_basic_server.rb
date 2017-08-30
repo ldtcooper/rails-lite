827391781f15761a86a26ec26f17a7fc6264ba33
Logan Cooper <ldtcoop@gmail.com>
2017-08-30 15:27:13 -0700

    Finish Phase 1

--- bin/p01_basic_server.rb
+++ bin/p01_basic_server.rb
@@ -1,1 +1,10 @@
 require 'rack'
+app = Proc.new do |env|
+  req = Rack::Request.new(env) #creates new request based off of what is passed into #call
+  res = Rack::Response.new #creates new resonse object (like a hash)
+  res['Content Type'] = 'text/html' # sets the content carried by response to html
+  res.write("#{req.path}") # writes content to response
+  res.finish # ends the response
+end
+
+Rack::Server.start(app: app, Port: 3000)
