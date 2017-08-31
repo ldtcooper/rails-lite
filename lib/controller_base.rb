6de21b2ffeb34a7a6b34dfd118c27eb79e77b98d
Logan Cooper <ldtcoop@gmail.com>
2017-08-30 17:11:50 -0700

    Add #render method

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -1,7 +1,9 @@
 require 'active_support'
 require 'active_support/core_ext'
+require 'active_support/inflector'
 require 'erb'
 require_relative './session'
+require 'byebug'
 
 class ControllerBase
   attr_reader :req, :res, :params
@@ -39,6 +41,12 @@ class ControllerBase
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    raise Exception.new("Double render detected") if already_built_response?
+    path = path_finder(template_name)
+    content = File.read(path)
+    erb_content = ERB.new(content).result(binding)
+    render_content(erb_content, 'text/html')
+    @already_built_response = true
   end
 
   # method exposing a `Session` object
@@ -47,5 +55,12 @@ class ControllerBase
 
   # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
+  end
+
+  private
+
+  def path_finder(template_name)
+    controller_name = self.class.to_s.underscore
+    "views/#{controller_name}/#{template_name}.html.erb"
   end
 end
